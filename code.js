df3fad97f4b3f8e0dd61c58d950602ed46d19379
Caoimhe <18lyonca@wcsu.net>
2018-02-28 17:23:41 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,78 @@
+//mood buttons
+
+onEvent("button1", "click", function(){
+setScreen("screen2");
+});
+onEvent("button1", "click", function(event) {
+});
+onEvent("button2", "click", function(){
+setScreen("screen3");
+});
+onEvent("button2", "click", function(event) {
+});
+onEvent("button3", "click", function(){
+setScreen("screen4");
+});
+onEvent("button3", "click", function(event) {
+});
+onEvent("button4", "click", function(){
+setScreen("screen5");
+});
+onEvent("button4", "click", function(event) {
+});
+onEvent("button5", "click", function(){
+setScreen("screen6");
+});
+onEvent("button5", "click", function(event) {
+});
+onEvent("button6", "click", function(){
+setScreen("screen7");
+});
+onEvent("button6", "click", function(event) {
+});
+onEvent("button7", "click", function(){
+setScreen("screen8");
+});
+onEvent("button7", "click", function(event) {
+});
+onEvent("button8", "click", function(){
+setScreen("screen9");
+});
+onEvent("button8", "click", function(event) {
+});
+onEvent("button9", "click", function(){
+setScreen("screen10");
+});
+onEvent("button9", "click", function(event) {
+});
+
+//home screen buttons
+
+onEvent("homeButton1", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton2", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton3", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton4", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton5", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton6", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton7", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton8", "click", function(){
+setScreen("screen1");
+});
+onEvent("homeButton9", "click", function(){
+setScreen("screen1");
+});
+
